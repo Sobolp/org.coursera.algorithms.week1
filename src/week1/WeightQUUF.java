f16060f81f2a73af4c81a1a3f642bea7105a235e
Sobolp <pbolotov@gmail.com>
2016-01-27 01:03:22 +0200

    week1_2

--- src/week1/WeightQUUF.java
+++ src/week1/WeightQUUF.java
@@ -1,5 +1,9 @@
 package week1;
 
+import java.io.*;
+import java.util.Scanner;
+
+
 /**
  * Created by pavelbolotov on 1/26/16.
  */
@@ -47,6 +51,28 @@ public class WeightQUUF {
         }
         resalt +="}";
         return resalt;
+    }
+    public static void main(String[] args) throws IOException {
+        File inFile = new File ("./txt/Question2.txt");
+
+        Scanner sc = new Scanner (inFile);
+        int N  = Integer.parseInt(sc.nextLine());
+        WeightQUUF uf = new WeightQUUF(N);
+        while (sc.hasNextLine())
+        {
+            String line = sc.nextLine();
+            String[] words = line.split("[\\s]");
+//            System.out.println (words[0]);
+            int p = Integer.parseInt(words[0]);
+            int q = Integer.parseInt(words[1]);
+            if (!uf.connected(p,q)){
+                uf.union(p,q);
+                System.out.println(p+" "+q);
+                System.out.println(uf);
+            }
+        }
+        sc.close();
+        System.out.println(uf);
     }
 
 }
