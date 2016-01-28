1bd95d83dd2b3369623ada056ac1fbff8b007234
Sobolp <pbolotov@gmail.com>
2016-01-29 00:04:31 +0200

    v 0.5 (fix  backwash)

--- src/week1/Percolation.java
+++ src/week1/Percolation.java
@@ -14,12 +14,14 @@ public class Percolation {
     private int pN;
     private int[][] grid;
     private WeightedQuickUnionUF unions;
+    private WeightedQuickUnionUF unionsAfter;
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
+        if (N <= 0) throw new IllegalArgumentException("N ≤ 0");
         this.pN = N + 2;
         this.unions = new WeightedQuickUnionUF(N * N + 2);
-        if (N <= 0) throw new IllegalArgumentException("N ≤ 0");
+        this.unionsAfter = new WeightedQuickUnionUF(N * N + 1);
         grid = new int[pN][pN];
         int count = 1;
         for (int i = 0; i < pN; i++) {
@@ -35,6 +37,7 @@ public class Percolation {
         for (int i = 1; i <= N; i++) {
             unions.union(0, i);
             unions.union(N * N + 1, N * N + 1 - i);
+            unionsAfter.union(0, i);
         }
     }
 
@@ -47,18 +50,22 @@ public class Percolation {
             if (i > 0)
                 if (this.isOpen(i - 1, j)) {
                     unions.union(point, point - pN + 2);
+                    unionsAfter.union(point, point - pN + 2);
                 }
             if (i < pN)
                 if (this.isOpen(i + 1, j)) {
                     unions.union(point, point + pN - 2);
+                    unionsAfter.union(point, point + pN - 2);
                 }
             if (j > 0)
                 if (this.isOpen(i, j - 1)) {
                     unions.union(point, point - 1);
+                    unionsAfter.union(point, point - 1);
                 }
             if (j < pN)
                 if (this.isOpen(i, j + 1)) {
                     unions.union(point, point + 1);
+                    unionsAfter.union(point, point + 1);
                 }
         }
     }
@@ -74,7 +81,10 @@ public class Percolation {
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
         if (isOpen(i, j))
-            return (unions.connected(0, Math.abs(grid[i][j])));
+            if (percolates())
+                return (unionsAfter.connected(0, Math.abs(grid[i][j])));
+            else
+                return (unions.connected(0, Math.abs(grid[i][j])));
         return false;
     }
 
@@ -84,19 +94,17 @@ public class Percolation {
         return (unions.connected(0, ((pN - 2) * (pN - 2) + 1)));
     }
 
-/*
-    @Override
+/*    @Override
     public String toString() {
-        String result ="";
-        for (int i=0;i<pN;i++) {
+        String result = "";
+        for (int i = 0; i < pN; i++) {
             for (int j = 0; j < pN; j++) {
-                result += grid[i][j]+"\t ";
+                result += grid[i][j] + "\t ";
             }
-            result +="\n";
+            result += "\n";
         }
         return result;
-    }
-*/
+    }*/
 
     // test client (optional)
     public static void main(String[] args) {
@@ -105,13 +113,13 @@ public class Percolation {
 
         a.open(1, 2);
         a.open(2, 2);
-        a.open(3, 3);
-        a.open(3, 4);
-        a.open(4, 4);
-        a.open(5, 4);
         a.open(3, 2);
+        a.open(4, 2);
+        a.open(5, 2);
+        a.open(5, 4);
+        //       a.open(3, 2);
         System.out.println(a);
-        System.out.println(a.isFull(4, 4));
+        System.out.println(a.isFull(5, 4));
         System.out.println(a.percolates());
 
     }
