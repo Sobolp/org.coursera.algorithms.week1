da4ff4ea581e235ee9bb1249599244fa53b5fa41
Sobolp <pbolotov@gmail.com>
2016-01-29 21:08:40 +0200

    v 1.0 (Ok ) Compilation:  PASSED Style:        PASSED Findbugs:     No potential bugs found. API:          PASSED
    Correctness:  26/26 tests passed
    Memory:       9/8 tests passed
    Timing:       9/9 tests passed
    
    Aggregate score: 101.25% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- src/week1/Percolation.java
+++ src/week1/Percolation.java
@@ -1,10 +1,10 @@
-//package week1;
-//
-//
-//import edu.princeton.cs.algorithms.WeightedQuickUnionUF;
-//
+package week1;
 
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import edu.princeton.cs.algorithms.WeightedQuickUnionUF;
+
+
+//import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
  * Created by SoBoLp on 1/27/16.
@@ -12,65 +12,59 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int pN;
-    private int[][] grid;
+    private MyGrid myGrid;
     private WeightedQuickUnionUF unions;
-    private WeightedQuickUnionUF unionsAfter;
+//    private WeightedQuickUnionUF unionsAfter;
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
         if (N <= 0) throw new IllegalArgumentException("N ≤ 0");
         this.pN = N + 1;
         this.unions = new WeightedQuickUnionUF(N * N + 2);
-        this.unionsAfter = new WeightedQuickUnionUF(N * N + 1);
-        grid = new int[pN][pN];
-        int count = 1;
-        for (int i = 0; i < pN; i++) {
-            for (int j = 0; j < pN; j++) {
-                if (i == 0 || i == pN || j == 0 || j == pN) {
-                    grid[i][j] = 0;
-                } else {
-                    grid[i][j] = count;
-                    count++;
-                }
+//        this.unionsAfter = new WeightedQuickUnionUF(N * N + 1);
+        myGrid = new MyGrid(N);
+        for (int i = 1; i < pN; i++) {
+            for (int j = 1; j < pN; j++) {
+                myGrid.setGrid(i, j, (byte) 0);
             }
         }
-//        if (N > 1) {
         for (int i = 1; i <= N; i++) {
             unions.union(0, i);
             unions.union(N * N + 1, N * N + 1 - i);
-            unionsAfter.union(0, i);
-//            }
+//            unionsAfter.union(0, i);
         }
     }
 
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) {
-//        if (i <= 0 || i > pN - 1) throw new IndexOutOfBoundsException("row index i out of bounds");
-//        if (j <= 0 || j > pN - 1) throw new IndexOutOfBoundsException("row index j out of bounds");
         if (!this.isOpen(i, j)) {
-            int point = grid[i][j];
-            grid[i][j] *= -1;
-
+            int point = myGrid.get1Dindex(i, j);
             if (i > 1)
                 if (this.isOpen(i - 1, j)) {
                     unions.union(point, point - pN + 1);
-                    unionsAfter.union(point, point - pN + 1);
+//                    unionsAfter.union(point, point - pN + 1);
                 }
             if (i < pN - 1)
                 if (this.isOpen(i + 1, j)) {
                     unions.union(point, point + pN - 1);
-                    unionsAfter.union(point, point + pN - 1);
+//                    unionsAfter.union(point, point + pN - 1);
                 }
             if (j > 1)
                 if (this.isOpen(i, j - 1)) {
                     unions.union(point, point - 1);
-                    unionsAfter.union(point, point - 1);
+//                    unionsAfter.union(point, point - 1);
                 }
             if (j < pN - 1)
                 if (this.isOpen(i, j + 1)) {
                     unions.union(point, point + 1);
-                    unionsAfter.union(point, point + 1);
+//                    unionsAfter.union(point, point + 1);
                 }
+            if (i == 1)
+                myGrid.setStatus(i, j, (byte) 3);
+            else if (i == pN - 1)
+                myGrid.setStatus(i, j, (byte) 2);
+            else
+                myGrid.setStatus(i, j, (byte) 1);
         }
     }
 
@@ -78,25 +72,16 @@ public class Percolation {
     public boolean isOpen(int i, int j) {
         if (i <= 0 || i > pN - 1) throw new IndexOutOfBoundsException("row index i out of bounds");
         if (j <= 0 || j > pN - 1) throw new IndexOutOfBoundsException("row index j out of bounds");
-        boolean result = false;
-        if (grid[i][j] < 0)
-            result = true;
-        return result;
+        return myGrid.isOpen(i, j);
     }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
         if (isOpen(i, j)) {
-//            if (pN - 1 == 1)
-//                return true;
-            if (percolates())
-                return (unionsAfter.connected(0, Math.abs(grid[i][j])));
-            else
-                return (unions.connected(0, Math.abs(grid[i][j])));
+            return myGrid.isFull(i, j);
         }
         return false;
     }
-
 
     // does the system percolate?
     public boolean percolates() {
@@ -105,12 +90,91 @@ public class Percolation {
         return (unions.connected(0, ((pN - 1) * (pN - 1) + 1)));
     }
 
+    private class MyGrid {
+        // 0 - close
+        // > 0 - is open
+        // 3 - connected top
+        // 2 - connected bottom
+        // 1 - no top no bottom just oppend
+        private byte[][] grid;
+
+        private MyGrid(int N) {
+            grid = new byte[N][N];
+        }
+
+        private int get1Dindex(int i, int j) {
+            return (grid[0].length * (i - 1) + (j - 1)) + 1;
+        }
+
+        private byte getGrid(int i, int j) {
+            return grid[i - 1][j - 1];
+        }
+
+        private void setGrid(int i, int j, byte val) {
+            this.grid[i - 1][j - 1] = val;
+        }
+
+        private void setStatus(int i, int j, byte val) {
+            setGrid(i, j, val);
+            byte nStat;
+            if (i > 1)
+                if (isOpen(i - 1, j)) {
+                    nStat = getGrid(i - 1, j);
+                    if (nStat < val)
+                        setStatus(i - 1, j, val);
+                    else if (nStat > val)
+                        setStatus(i, j, nStat);
+                }
+            if (i < pN - 1)
+                if (isOpen(i + 1, j)) {
+                    nStat = getGrid(i + 1, j);
+                    if (nStat < val)
+                        setStatus(i + 1, j, val);
+                    else if (nStat > val)
+                        setStatus(i, j, nStat);
+                }
+            if (j > 1)
+                if (isOpen(i, j - 1)) {
+                    nStat = getGrid(i, j - 1);
+                    if (nStat < val)
+                        setStatus(i, j - 1, val);
+                    else if (nStat > val)
+                        setStatus(i, j, nStat);
+                }
+            if (j < pN - 1)
+                if (isOpen(i, j + 1)) {
+                    nStat = getGrid(i, j + 1);
+                    if (nStat < val)
+                        setStatus(i, j + 1, val);
+                    else if (nStat > val)
+                        setStatus(i, j, nStat);
+                }
+        }
+
+        private boolean isOpen(int i, int j) {
+            boolean result = false;
+            if (getGrid(i, j) > 0)
+                result = true;
+            return result;
+        }
+
+        private boolean isFull(int i, int j) {
+            boolean resalt = false;
+//            if (!percolates()) {
+            if (getGrid(i, j) == 3)
+                return true;
+//            } else if (getGrid(i, j) == 1 || getGrid(i, j) == 2)
+//                return true;
+            return resalt;
+        }
+    }
+
 /*    @Override
     public String toString() {
         String result = "";
-        for (int i = 0; i < pN; i++) {
-            for (int j = 0; j < pN; j++) {
-                result += grid[i][j] + "\t ";
+        for (int i = 1; i < pN; i++) {
+            for (int j = 1; j < pN; j++) {
+                result += myGrid.getGrid(i, j) + "\t ";
             }
             result += "\n";
         }
@@ -119,17 +183,22 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation a = new Percolation(1);
+        Percolation a = new Percolation(3);
 
 
-        a.open(1, 1);
-//        a.open(2, 2);
+//        a.open(1, 1);
+
 //        a.open(3, 2);
 //        a.open(4, 2);
 //        a.open(5, 2);
 //        a.open(6, 5);
-//        //       a.open(3, 2);
-//        System.out.println(a);
+        a.open(1, 1);
+        a.open(1, 2);
+        a.open(2, 2);
+
+        a.open(2, 1);
+        a.open(3, 1);
+        System.out.println(a);
         System.out.println(a.isFull(1, 1));
         System.out.println(a.percolates());
 
