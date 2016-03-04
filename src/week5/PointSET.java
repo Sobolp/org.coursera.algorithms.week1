e10c02cca8f5d68c639663c7fe95b4d509879b14
Sobolp <pbolotov@gmail.com>
2016-03-04 13:09:23 +0200

    week5 v0.0.1 only PointSET

---
+++ src/week5/PointSET.java
@@ -0,0 +1,104 @@
+package week5;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.TreeSet;
+
+/**
+ * Created by SoBoLp on 3/4/16.
+ */
+public class PointSET {
+    private TreeSet<Point2D> point2DTreeSet;
+
+    /** construct an empty set of points
+     *
+     */
+    public PointSET(){
+        point2DTreeSet = new TreeSet<>();
+    }
+
+    /** is the set empty?
+     *
+     * @return
+     */
+    public boolean isEmpty(){
+        return point2DTreeSet.isEmpty();
+    }
+
+    /** number of points in the set
+     *
+     * @return
+     */
+    public int size(){
+        return point2DTreeSet.size();
+    }
+
+    /** add the point to the set (if it is not already in the set)
+     *
+     * @param p
+     */
+    public void insert(Point2D p){
+        if (p == null) throw new NullPointerException ("argument is null");
+        point2DTreeSet.add(p);
+    }
+
+    /** does the set contain point p?
+     *
+     * @param p
+     * @return
+     */
+    public boolean contains(Point2D p){
+        return point2DTreeSet.contains(p);
+    }
+
+    /** draw all points to standard draw
+     *
+     */
+    public void draw(){
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.01);
+        for (Point2D p : point2DTreeSet){
+            StdDraw.point(p.x(),p.y());
+        }
+    }
+
+    /** all points that are inside the rectangle
+     *
+     * @param rect
+     * @return
+     */
+    public Iterable<Point2D> range(RectHV rect){
+        Stack<Point2D> stack = new Stack<>();
+        for (Point2D p : point2DTreeSet){
+            if (rect.contains(p))
+                stack.push(p);
+        }
+        return stack;
+    }
+
+    /** a nearest neighbor in the set to point p; null if the set is empty
+     *
+     * @param p
+     * @return
+     */
+    public Point2D nearest(Point2D p){
+        Point2D nearestPoint = new Point2D(2.0,2.0);
+        for (Point2D thisP : point2DTreeSet){
+            if (p.distanceTo(thisP)< p.distanceTo(nearestPoint)){
+                nearestPoint = thisP;
+            }
+        }
+        return nearestPoint;
+    }
+
+    /** unit testing of the methods (optional)
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ tests/week5/PointSETTest.java
@@ -0,0 +1,97 @@
+package week5;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SoBoLp on 3/4/16.
+ */
+public class PointSETTest {
+
+    PointSET empty;
+    PointSET circle4;
+    PointSET circle10;
+
+
+    private Iterable<Point2D> loadPoints(String fileName) throws FileNotFoundException {
+        Stack<Point2D> points = new Stack<>();
+        File inFile = new File(fileName);
+        Scanner sc = new Scanner(inFile);
+        while (sc.hasNext()) {
+            float x = sc.nextFloat();
+            float y = sc.nextFloat();
+            points.push(new Point2D(x, y));
+        }
+        return points;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        empty = new PointSET();
+        circle4 = new PointSET();
+        for (Point2D p : loadPoints("./txt/week5/circle4.txt"))
+            circle4.insert(p);
+        circle10 = new PointSET();
+        for (Point2D p : loadPoints("./txt/week5/circle10.txt"))
+            circle10.insert(p);
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        assertTrue("empty",empty.isEmpty());
+        assertFalse("circle4.txt",circle4.isEmpty());
+        assertFalse("circle10.txt",circle10.isEmpty());
+    }
+
+    @Test
+    public void testSize() throws Exception {
+        assertEquals("empty",0,empty.size());
+        assertEquals("circle4.txt",4,circle4.size());
+        assertEquals("circle10.txt",10,circle10.size());
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        try {
+            empty.insert(null);
+            fail("Check out of bounds");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @Test
+    public void testContains() throws Exception {
+        assertTrue("circle4.txt contains Point (0.5,1)",circle4.contains(new Point2D(0.5,1)));
+        assertFalse("circle4.txt not contains Point (0.6,0.6)",circle4.contains(new Point2D(0.6,0.6)));
+        assertTrue("circle10.txt contains Point (0.5,1)",circle10.contains(new Point2D(0.5,1)));
+        assertFalse("circle10.txt not contains Point (0.6,0.6)",circle10.contains(new Point2D(0.6,0.6)));
+    }
+
+    @Test
+    public void testRange() throws Exception {
+        assertEquals("circle4.txt containt all 4 points in rect((0.0,0.0),(1.0,1.0))"
+                ,"(0.5, 1.0) (1.0, 0.5) (0.0, 0.5) (0.5, 0.0) ",circle4.range(new RectHV(0.0,0.0,1.0,1.0)).toString());
+        assertEquals("circle10.txt containt 3 points in rect((0.0,0.0),(0.4,0.4))"
+                ,"(0.02447200007736683, 0.34549200534820557) (0.2061070054769516, 0.09549199789762497) "
+                ,circle10.range(new RectHV(0.0,0.0,0.4,0.4)).toString());
+    }
+
+    @Test
+    public void testNearest() throws Exception {
+        assertEquals("circle4.txt nearest point to Point(0.1,0.6) is Point(0.0,0.5)",
+                "(0.0, 0.5)",circle4.nearest(new Point2D(0.1,0.6)).toString());
+        assertEquals("circle10.txt nearest point to Point(0.0,0.0) is Point(0.206107 0.095492)",
+                "(0.2061070054769516, 0.09549199789762497)",circle10.nearest(new Point2D(0.0,0.0)).toString());
+
+    }
+}
